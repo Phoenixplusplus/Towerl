08643147c950302a054406eda69854f7f7eadea1
Phoenix <phoenixmee91@gmail.com>
2018-09-27 15:14:31 +0100

    Camera, Ball, Tower, Phoenix Scene

---
+++ Towerl/Assets/Scenes/Phoenix/PhoenixScripts/BallPhysics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+
+    [Header("Bounce control Variables")]
+    public float Gravity = -10.0f;
+
+    private float StartHeight, segHeight;
+    private int MaxBounceHeight;
+    private Vector3 Speed = new Vector3(0, 0, 0);
+    private GameObject Tower;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject Tower = GameObject.Find("Tower");
+        TowerController TowerController = Tower.GetComponent<TowerController>();
+
+        MaxBounceHeight = TowerController.segmentspace;
+        //StartHeight = TowerController.clone.GetComponent<MeshRenderer>().bounds.size.y;
+        StartHeight = TowerController.clone.transform.localScale.y + MaxBounceHeight;
+        segHeight = TowerController.segHeight;
+
+        transform.position = new Vector3(transform.position.x, StartHeight, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Speed.y += Gravity * Time.fixedDeltaTime;
+        transform.position += Speed * Time.fixedDeltaTime;
+
+        if (transform.position.y <= StartHeight - MaxBounceHeight + segHeight && Speed.y < 0)
+        {
+            Speed.y *= -1;
+        }
+
+    }
+}
---
+++ Towerl/Assets/Scenes/Phoenix/PhoenixScripts/TowerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+
+    [Header("Level Data")]
+    private int levels = 5;
+    public int[,] data = new int[5, 12]
+    {
+        {1,1,1,1,1,1,0,0,1,1,1,1 },
+        {1,1,1,1,0,0,0,0,1,1,0,0 },
+        {1,1,1,1,0,0,0,0,1,1,1,1 },
+        {0,0,1,1,0,0,1,1,1,1,1,1 },
+        {1,1,0,0,0,1,0,0,1,1,1,1 }
+    };
+
+
+    [Header("Segment Pre-fab")]
+    public Transform segment;
+    public Transform column;
+
+    [Header("Level Attributes")]
+    public int segmentspace;
+
+        public Transform clone;
+        public float segHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        // make column
+        clone = (Transform)Instantiate(column, new Vector3(0, (float)levels / 2, 0), Quaternion.identity);
+        clone.transform.localScale = new Vector3(1.0f, 10.0f, 1.0f);
+
+        // make each layer
+        for (int level = 4; level >= 0; level--)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (data[level, i] == 1)
+                {
+                    Transform segClone = (Transform)Instantiate(segment, new Vector3(0, clone.transform.localScale.y - level * segmentspace, 0), Quaternion.Euler(0, i * 30, 0));
+                    segClone.transform.localScale = new Vector3(300.0f, 10.0f, 300.0f);
+                    segHeight = segClone.GetComponent<MeshRenderer>().bounds.size.y;
+                    segClone.gameObject.tag = level.ToString();
+                    segClone.transform.parent = clone.transform;
+                }
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
