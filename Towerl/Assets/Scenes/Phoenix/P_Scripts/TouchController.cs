237691efb443e79eef84e5809a72372eb69a8693
Phoenix <phoenixmee91@gmail.com>
2018-10-04 17:10:13 +0100

    Touch Controller

---
+++ Towerl/Assets/Scenes/Phoenix/P_Scripts/TouchController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+    Touch userTouch;
+    Vector2 userRotation;
+    int userSensitivity = 1;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        userTouch = Input.GetTouch(0);
+        userRotation = userTouch.deltaPosition * userTouch.deltaTime * userSensitivity;
+	}
+}
