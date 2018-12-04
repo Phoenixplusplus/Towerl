b2924c715bee85fbc757458b28cbdd7089ae9a95
Max Bryans <maxsfw3@gmail.com>
2018-12-04 17:24:49 +0000

    New MENU ... yeeeehaaaaa

--- Towerl/Assets/Scripts/BUILD_SCRIPTS/Menu_Control.cs
+++ Towerl/Assets/Scripts/BUILD_SCRIPTS/Menu_Control.cs
@@ -11,6 +11,14 @@ public class Menu_Control : MonoBehaviour {
     private Vector3 vel = new Vector3(0f, 0f, 0f);
     public Vector3 G = new Vector3(0, -0.25f, 0);
     public RectTransform Title;
+    public CanvasGroup Select_Menu_Canvas;
+    public Camera Main_Camera;
+    public Camera Select_Menu_Camera;
+    public UI_Manager UI_Manager;
+    public LevelManager Level_Manager;
+    public MGC Controller;
+    public CameraController2 CamCon2;
+    public Canvas Level_Choose;
     public float PhaseInTime = 2f;
     public float StartTime = 0f;
     private float Angle = 0;
@@ -18,9 +26,39 @@ public class Menu_Control : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(PhaseIn());
+        Enter();
 	}
 	
+    public void Enter()
+    {
+        CamTog(Main_Camera, false);
+        CamTog(Select_Menu_Camera, true);
+        GUITog(Select_Menu_Canvas, true);
+        ball.position = new Vector3(-10f, 0.8f, -9.34f);
+        vel = new Vector3(0f, 0f, 0f);
+        StartCoroutine(PhaseIn());
+    }
+
+    public void Leave()
+    {
+        Main_Camera.enabled = true;
+        Select_Menu_Camera.enabled = false;
+        GUITog(Select_Menu_Canvas, false);
+    }
+
+    void CamTog(Camera cam, bool status)
+    {
+        cam.enabled = status;
+        cam.GetComponent<AudioListener>().enabled = status;
+    }
+
+    void GUITog(CanvasGroup CG, bool status)
+    {
+        if (status) { CG.alpha = 1f; }
+        else { CG.alpha = 0f; }
+        CG.interactable = status;
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
@@ -44,11 +82,29 @@ public class Menu_Control : MonoBehaviour {
         }
         // rotate self (ie the column assembly)
         transform.localEulerAngles = new Vector3(0, Angle, 0);
+        // Debug.Log(Angle.ToString());
     }
 
-    void GO()
+    public void GO()
     {
-
+        Debug.Log("Go called");
+        if (Angle >= 30f && Angle < 150f) // STORY MODE
+        {
+            Leave();
+            Level_Choose.gameObject.SetActive(false);
+            CamCon2.EnableAdventureMap(true);
+        }
+        else if (Angle >=150f && Angle < 270f) // QUIT
+        {
+            UI_Manager.QuitGame();
+            Application.Quit();
+        }
+        else // CASUAL MODE       
+        {
+            Leave();
+            Level_Manager.SetGameMode(0);
+            Controller.PlayMe();
+        }
     }
 
 
@@ -62,6 +118,7 @@ public class Menu_Control : MonoBehaviour {
             Title.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, lerp);
             yield return null;
         }
+        StartTime = 0f;
         yield break;
     }
 }
